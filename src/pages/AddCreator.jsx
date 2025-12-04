cb4e64dce371b5ed91d56a9084d6fa3bb0c7eb59
Dinesh <dineshigdd@gmail.com>
2025-12-03 21:53:16 -0800

    add creator form

--- src/pages/AddCreator.jsx
+++ src/pages/AddCreator.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { supabase } from '../client'
 
+
 function AddCreator() {
+  const [ name, setName ] = useState("");
+  const [ url, setURL ] = useState("");
+  const [ description, setDescription ] = useState("");
+  const [ imageURL, setimageURL ] = useState("");
+
+  
+  const handleSubmit = async ( e )=>{
+    e.preventDefault();
+  
+    const { error } = await supabase
+      .from('creators')
+      .insert([
+         { name , url , description , imageURL }
+      ])
+    }
+  
+
   return (
-    <div>Add Creator
-      <form>
+    <div className='container'>
+      <form onSubmit={ handleSubmit }>
         <label>name
-          <input type='text' />
+          <input type='text' onChange={ ( e )=> setName( e.target.value )} />
         </label>
 
         <label>URL
-          <input type='text' />
+          <input type='text' onChange={ ( e )=> setURL( e.target.value )}/>
         </label>
 
 
         <label>description
-          <input type='text' />
+          <input type='text' onChange={ ( e )=> setDescription( e.target.value )}/>
         </label>
 
        <label>imageUrl
-          <input type='text' />
+          <input type='text'  onChange={ ( e )=> setimageURL( e.target.value )}/>
         </label>
+        <button type='submit'>Submit</button>
       </form>
 
 
